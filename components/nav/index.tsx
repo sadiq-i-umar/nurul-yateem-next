1a6f5d56e47136452902f4277f95b4a7fcdbf4a6
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-03-20 15:36:06 +0100

    Add Navigation Items

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -1,11 +1,32 @@
 import { Box } from "@mui/material";
+import { usePathname } from "next/navigation";
 
 const Nav: React.FC = () => {
+
+  const path = usePathname();
+
   return (
-    <Box>
+    <Box sx={{ paddingLeft: "5px", paddingTop: "50px" }}>
       {navConfig.map((item) => (
-        <NavItem key={item.title} icon={item.icon} title={item.title} />
+        <Box sx={{ mb: "40px", mr: "20px" }}>
+        <NavItem key={item.title} icon={item.icon} title={item.title} isActive={path === item.path ? (true) : (false)} />
+        </Box>
       ))}
+    </Box>
+  );
+};
+
+const NavItem: React.FC<{ icon: string; title: string, isActive: boolean }> = ({
+  icon,
+  title,
+  isActive
+}) => {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", cursor: "pointer", '&:hover': {color: "#237A00"}, ...(isActive == true && {backgroundColor: "#237A00", color: "#FFFFFF", "&:hover": {color: "#FFFFFF"}}), pt: "15px", pb: "10px", px: "10px", borderRadius: "10px" }}>
+      <Box sx={{ mr: {xs: "20px", md: "30px"} }}>
+        <NavIcon icon={icon}/>
+      </Box>
+      <Box sx={{ mt: "-6px" }}>{title}</Box>
     </Box>
   );
 };
@@ -27,25 +48,27 @@ const NavIcon: React.FC<{ icon: string }> = ({icon}) => {
   );
 };
 
-const NavItem: React.FC<{ icon: string; title: string }> = ({
-  icon,
-  title,
-}) => {
-  return (
-    <Box sx={{ display: "flex", alignItems: "center" }}>
-      <Box sx={{ mr: "10px" }}>
-        <NavIcon icon={icon}/>
-      </Box>
-      <Box>{title}</Box>
-    </Box>
-  );
-};
-
 const navConfig = [
   {
     title: "Dashboard",
     icon: "/category.svg",
+    path: "/dashboard/guardian"
   },
+  {
+    title: "Orphan List",
+    icon: "/users.svg",
+    path: "/dashboard/guardian/orphan-list"
+  },
+  {
+    title: "Activity",
+    icon: "/folder_open.svg",
+    path: "/dashboard/guardian/activity"
+  },
+  {
+    title: "Settings",
+    icon: "/gear.svg",
+    path: "/dashboard/guardian/settings"
+  }
 ];
 
 export default Nav;
--- src/app/(dashboard)/dashboard/guardian/layout.tsx
+++ src/app/(dashboard)/dashboard/guardian/layout.tsx
@@ -1,65 +1,166 @@
-'use client'
+"use client";
 
 import { Box } from "@mui/material";
 import { ImageFrameRectangular117By53 } from "../../../../../components/common/image-frames";
 import Image from "next/image";
 import { useState } from "react";
+import Nav from "../../../../../components/nav";
 
-const Layout:React.FC = () => {
+const Layout: React.FC = () => {
+  const [openNav, setOpenNav] = useState(true);
+  const [openSmallNav, setOpenSmallNav] = useState(false);
 
-    const [ openNav, setOpenNav ] = useState(true);
-    const [ openSmallNav, setOpenSmallNav ] = useState(false);
-
-    return (
-        <>
-            {/* <Header />
-            <Nav /> */}
-            <Box sx={{ display: "flex" }}>
-                {openNav && <Box sx={{ width: "280px", display: {xs: "none", md: "block"}, paddingLeft: "20px" }}>
-                    <Box sx={{ display: "flex", alignItems: "center", height: "90px", paddingRight: "15px"}}>
-                        <ImageFrameRectangular117By53 image="/nurul_yateem_logo.png"/>
-                        <Box onClick={() => setOpenNav(!openNav)} sx={{ flexGrow: 1, display: "flex", justifyContent: "flex-end", cursor: "pointer" }}><Image src={"/menu.svg"} width={24} height={24} alt={"Menu Icon"}/></Box>
-                    </Box>
-                    <Box sx={{ height: "100vh" }}>
-                        Nav Items
-                    </Box>
-                </Box>}
-                {openSmallNav && <Box sx={{ position: "fixed", zIndex: 10000, backgroundColor: 'rgba(0, 0, 0, 0.3)', width: "100%" }}><Box sx={{ width: "200px", display: {xs: "block", md: "none"}, paddingLeft: "20px", backgroundColor: "white" }}>
-                    <Box sx={{ display: "flex", alignItems: "center", height: "90px", paddingRight: "15px"}}>
-                        <ImageFrameRectangular117By53 image="/nurul_yateem_logo.png"/>
-                        <Box onClick={() => setOpenSmallNav(!openSmallNav)} sx={{ flexGrow: 1, display: "flex", justifyContent: "flex-end", cursor: "pointer" }}><Image src={"/menu.svg"} width={24} height={24} alt={"Menu Icon"}/></Box>
-                    </Box>
-                    <Box sx={{ height: "100vh" }}>
-                        Nav Items
-                    </Box>
-                </Box></Box>}
-                <Box sx={{ flexGrow: 1 }}>
-                    <Box sx={{ height: "90px", display: "flex", alignItems: "center" }}>
-                        <Box onClick={() => setOpenNav(!openNav)} sx={{ flexGrow: 1, cursor: "pointer", ...(openNav == false ? {display: {xs: "none", md: "block"}} : {display: "none"}) }}>
-                            <Image src={"/menu.svg"} width={24} height={24} alt={"Menu Icon"}/>
-                        </Box>
-                        <Box onClick={() => setOpenSmallNav(!openSmallNav)} sx={{ flexGrow: 1, cursor: "pointer", ...(openSmallNav == false ? {display: {xs: "block", md: "none"}} : {display: "none"}) }}>
-                            <Image src={"/menu.svg"} width={24} height={24} alt={"Menu Icon"}/>
-                        </Box>
-                        <Box sx={{ flexGrow: 1, justifyContent: "flex-end", display: "flex" }}>
-                            <Box>
-                                <Image alt={"Search Icon"} width={24} height={24} src={"/search_icon.svg"}/>
-                            </Box>
-                            <Box>
-                                <Image alt={"Message Icon"} width={24} height={24} src={"/message.svg"}/>
-                            </Box>
-                            <Box>
-                                <Image alt={"Bell Icon"} width={24} height={24} src={"/bell.svg"}/>
-                            </Box>
-                        </Box>
-                    </Box>
-                    <Box sx={{ height: "100vh", backgroundColor: "#F5F5F5" }}>
-                        Main Content
-                    </Box>
-                </Box>
+  return (
+    <Box sx={{ display: "flex" }}>
+      {openNav && (
+        <Box
+          sx={{
+            width: "280px",
+            display: { xs: "none", md: "block" },
+            paddingLeft: "20px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              height: "90px",
+              paddingRight: "15px",
+            }}
+          >
+            <ImageFrameRectangular117By53 image="/nurul_yateem_logo.png" />
+            <Box
+              onClick={() => setOpenNav(!openNav)}
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                justifyContent: "flex-end",
+                cursor: "pointer",
+              }}
+            >
+              <Image
+                src={"/menu.svg"}
+                width={24}
+                height={24}
+                alt={"Menu Icon"}
+              />
             </Box>
-        </>
-    );
-}
+          </Box>
+          <Box sx={{ height: "100vh" }}>
+            <Nav />
+          </Box>
+        </Box>
+      )}
+      {openSmallNav && (
+        <Box
+          sx={{
+            position: "fixed",
+            zIndex: 10000,
+            backgroundColor: "rgba(0, 0, 0, 0.3)",
+            width: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              width: {xs: "200px", sm: "250px"},
+              display: { xs: "block", md: "none" },
+              paddingLeft: "20px",
+              backgroundColor: "white",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                height: "90px",
+                paddingRight: "15px",
+              }}
+            >
+              <ImageFrameRectangular117By53 image="/nurul_yateem_logo.png" />
+              <Box
+                onClick={() => setOpenSmallNav(!openSmallNav)}
+                sx={{
+                  flexGrow: 1,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  cursor: "pointer",
+                }}
+              >
+                <Image
+                  src={"/menu.svg"}
+                  width={24}
+                  height={24}
+                  alt={"Menu Icon"}
+                />
+              </Box>
+            </Box>
+            <Box sx={{ height: "100vh" }}>
+                <Nav />
+            </Box>
+          </Box>
+        </Box>
+      )}
+      <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ height: "90px", display: "flex", alignItems: "center" }}>
+          <Box
+            onClick={() => setOpenNav(!openNav)}
+            sx={{
+              flexGrow: 1,
+              cursor: "pointer",
+              ...(openNav == false
+                ? { display: { xs: "none", md: "block" } }
+                : { display: "none" }),
+            }}
+          >
+            <Image src={"/menu.svg"} width={24} height={24} alt={"Menu Icon"} />
+          </Box>
+          <Box
+            onClick={() => setOpenSmallNav(!openSmallNav)}
+            sx={{
+              flexGrow: 1,
+              cursor: "pointer",
+              ...(openSmallNav == false
+                ? { display: { xs: "block", md: "none" } }
+                : { display: "none" }),
+            }}
+          >
+            <Image src={"/menu.svg"} width={24} height={24} alt={"Menu Icon"} />
+          </Box>
+          <Box
+            sx={{ flexGrow: 1, justifyContent: "flex-end", display: "flex" }}
+          >
+            <Box>
+              <Image
+                alt={"Search Icon"}
+                width={24}
+                height={24}
+                src={"/search_icon.svg"}
+              />
+            </Box>
+            <Box>
+              <Image
+                alt={"Message Icon"}
+                width={24}
+                height={24}
+                src={"/message.svg"}
+              />
+            </Box>
+            <Box>
+              <Image
+                alt={"Bell Icon"}
+                width={24}
+                height={24}
+                src={"/bell.svg"}
+              />
+            </Box>
+          </Box>
+        </Box>
+        <Box sx={{ height: "100vh", backgroundColor: "#F5F5F5" }}>
+          Main Content
+        </Box>
+      </Box>
+    </Box>
+  );
+};
 
 export default Layout;
