84a84770cd154c8c3165cdcdb73f4939cddb9a97
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2024-04-02 18:42:37 +0100

    fix error

--- components/orphan-list/page/index.tsx
+++ components/orphan-list/page/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { Box } from "@mui/material";
 import SubHeader from "../../sub-header";
@@ -9,40 +9,42 @@ import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
 const OrphanListPage: React.FC = () => {
+  const { data: session } = useSession();
+  const token = session?.token;
 
-    const {data: session} = useSession()
+  const { data } = useQuery({
+    queryKey: ["orphans"],
+    queryFn: () => getOrphans(token),
+    enabled: !!token,
+  });
 
-    const { data } = useQuery({
-        queryKey: ["orphans"],
-        queryFn: () => getOrphans(session?.token)
-    })
+  useEffect(() => {
+    if (data) {
+      console.log(JSON.stringify(data.orphans));
+    }
+  }, [data]);
 
-    useEffect(() => {
-        if (data) {
-            console.log(JSON.stringify(data.orphans))
-        }
-    }, [data])
-
-    return (
-        <Box>
-            <Box>
-                <h1>Orphan List</h1>
-            </Box>
-            <Box>
-                <SubHeader 
-                    title={""} 
-                    subtitle={""} 
-                    itemCount={undefined}
-                    buttonTwoText={"Add Orphans"}
-                    buttonTwoIcon="/plus.svg"
-                    itemCountLabel={""} 
-                    pageHasTable={true} />
-            </Box>
-            <Box sx={{ marginX: "-30px" }}>
-                <OrphanListTable orphanData={data?.orphans}/>
-            </Box>
-        </Box>
-    );
-}
+  return (
+    <Box>
+      <Box>
+        <h1>Orphan List</h1>
+      </Box>
+      <Box>
+        <SubHeader
+          title={""}
+          subtitle={""}
+          itemCount={undefined}
+          buttonTwoText={"Add Orphans"}
+          buttonTwoIcon="/plus.svg"
+          itemCountLabel={""}
+          pageHasTable={true}
+        />
+      </Box>
+      <Box sx={{ marginX: "-30px" }}>
+        <OrphanListTable orphanData={data?.orphans} />
+      </Box>
+    </Box>
+  );
+};
 
 export default OrphanListPage;
