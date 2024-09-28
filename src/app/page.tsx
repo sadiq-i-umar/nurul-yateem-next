d105a2c0cebc93813951d452392ca265e4c97787
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-09-28 17:39:14 +0100

    Clean up dummy code in root path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { CircularProgress, LinearProgress } from "@mui/material";
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main>
-      <h1>Hello This is no longer empty</h1>
-      <LinearProgress color="secondary" />
-    </main>
-  );
+  return <main></main>;
 }
