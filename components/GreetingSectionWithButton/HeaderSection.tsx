b9c0629d04403ce1d4029c3dacda8fc72279298c
Sadiq Umar <umarsadiqibrahim@gmail.com>
2024-11-18 23:21:56 +0100

    Show user name in dashboard page greeting

--- components/GreetingSectionWithButton/HeaderSection.tsx
+++ components/GreetingSectionWithButton/HeaderSection.tsx
@@ -1,47 +1,49 @@
-"use client";
-import { Box, Button, Skeleton, Typography } from "@mui/material";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+'use client';
+import { Box, Button, Skeleton, Typography } from '@mui/material';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function HeaderSection() {
   const router = useRouter();
   const { data: session } = useSession();
-  const firstName = session?.user?.firstName;
-  const lastName = session?.user?.lastName;
-  const accountType = session?.user?.accountType;
+  const firstName = session?.user?.profile?.firstName;
+  const lastName = session?.user?.profile?.lastName;
+  const accountType = session?.user?.profile?.accountType;
 
   // Get current hour
   const currentHour = new Date().getHours();
-  let greeting = "";
+  let greeting = '';
   if (currentHour < 12) {
-    greeting = "Good morning";
+    greeting = 'Good morning';
   } else if (currentHour < 18) {
-    greeting = "Good afternoon";
+    greeting = 'Good afternoon';
   } else {
-    greeting = "Good evening";
+    greeting = 'Good evening';
   }
 
   return (
     <Box
       sx={{
-        display: "flex",
-        marginTop: "40px",
-        marginBottom: "30px",
-        flexDirection: { xs: "column", sm: "row" },
+        display: 'flex',
+        marginTop: '40px',
+        marginBottom: '30px',
+        flexDirection: { xs: 'column', sm: 'row' },
       }}
     >
-      <Box sx={{ flexGrow: 1, marginBottom: { xs: "20px", sm: "0px" } }}>
+      <Box sx={{ flexGrow: 1, marginBottom: { xs: '20px', sm: '0px' } }}>
         <Box>
-          <Typography sx={{ fontSize: "18px", textTransform: "capitalize" }}>
-            {`${greeting},`}{" "}
+          <Typography sx={{ fontSize: '18px', textTransform: 'capitalize' }}>
+            {`${greeting},`}{' '}
             {firstName ? (
-              <span>{`${firstName} ${lastName}`}</span>
+              <span
+                style={{ fontWeight: 700 }}
+              >{`${firstName} ${lastName}`}</span>
             ) : (
               <Skeleton
-                sx={{ display: "inline-block" }}
-                variant="text"
-                animation="pulse"
+                sx={{ display: 'inline-block' }}
+                variant='text'
+                animation='pulse'
                 width={140}
                 height={24}
               />
@@ -49,39 +51,39 @@ export default function HeaderSection() {
           </Typography>
         </Box>
         <Box>
-          <Typography sx={{ fontSize: "14px" }}>
+          <Typography sx={{ fontSize: '14px' }}>
             Explore and build your foundation
           </Typography>
         </Box>
       </Box>
       <Box>
         <Button
-          variant="contained"
+          variant='contained'
           disableElevation
           sx={{
-            textTransform: "none",
-            borderRadius: "30px",
-            paddingX: "20px",
-            paddingY: "10px",
-            backgroundColor: "#3863FA",
+            textTransform: 'none',
+            borderRadius: '30px',
+            paddingX: '20px',
+            paddingY: '10px',
+            backgroundColor: '#3863FA',
             zIndex: 0,
-            "&:hover": {
-              backgroundColor: "#3863FA",
+            '&:hover': {
+              backgroundColor: '#3863FA',
             },
           }}
           onClick={() => {
-            if (accountType === "SPONSOR") {
-              router.push("/dashboard");
+            if (accountType === 'SPONSOR') {
+              router.push('/dashboard');
             } else {
-              router.push("/dashboard/guardian/orphan-list/add-an-orphan");
+              router.push('/dashboard/guardian/orphan-list/add-an-orphan');
             }
           }}
         >
-          {accountType == "SPONSOR" ? (
+          {accountType == 'SPONSOR' ? (
             <Typography>Donate to an orphan</Typography>
           ) : (
             <>
-              <Image src="/plus.svg" width={20} height={20} alt={"Plus Icon"} />
+              <Image src='/plus.svg' width={20} height={20} alt={'Plus Icon'} />
               <Typography>Add an Orphan</Typography>
             </>
           )}
