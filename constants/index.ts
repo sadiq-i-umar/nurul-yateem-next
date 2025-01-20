2c89b1cd523c6a2fe98fbd859fd5ac50d29b3bbd
Sadiq Umar <umarsadiqibrahim@gmail.com>
2025-01-20 03:59:36 +0100

    change api base url

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,7 @@
 //constants
 
-export const baseUrl = 'http://localhost:3002/api';
+// export const baseUrl = 'http://localhost:3002/api';
+export const baseUrl = "https://projecthope.onrender.com/api";
 
 export const api = {
   addOrphan: `${baseUrl}/v1/orphan`,
