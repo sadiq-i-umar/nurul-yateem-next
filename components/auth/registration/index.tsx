6c805ddfbc0a4968c40cf95a489475dbe81f893c
Sadiq Umar <umarsadiqibrahim@gmail.com>
2025-06-05 05:36:42 +0100

    style: start registration ui

--- components/auth/registration/index.tsx
+++ components/auth/registration/index.tsx
@@ -4,7 +4,19 @@ import useRegistrationForm from "./form";
 
 const Register = () => {
   const { form } = useRegistrationForm();
-  return <Form {...form} />;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 ">
+      <div></div>
+      <div>
+        <div className="mb-10">
+          <h1>Get Started</h1>
+          <p>Create your account now</p>
+        </div>
+        <Form {...form} />
+        <p className="text-center mt-2">You already have an account? Login</p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
